040431cf86c4d1edfbb35cb7b722f356bb0d850f
imkarimkarim <allalirezamail@gmail.com>
2022-11-09 22:41:38 +0330

    :speech_balloon: update social links

--- src/lib/Socials.svelte
+++ src/lib/Socials.svelte
@@ -13,22 +13,18 @@
   <p class="pb-5">{msg("socials")}</p>
   <div class="flex w-100 items-center justify-between pr-14 pl-14">
     <Link href="https://github.com/imkarimkarim">
-      <img
-        class="{imageClass} dark:invert"
-        src={github}
-        alt="github icon"
-      />
+      <img class="{imageClass} dark:invert" src={github} alt="github icon" />
     </Link>
     <Link href="https://instagram.com/imkarimkarim">
       <img class={imageClass} src={instagram} alt="instagram icon" />
     </Link>
-    <Link href="https://www.linkedin.com/in/alireza-madani-670475245/">
+    <Link href="https://www.linkedin.com/in/imkarimkarim">
       <img class={imageClass} src={linkedin} alt="linkedin icon" />
     </Link>
     <Link href="https://twitter.com/imkarimkarim">
       <img class={imageClass} src={twitter} alt="twitter icon" />
     </Link>
-    <Link href="https://www.youtube.com/channel/UCB7I8lUcRo5-45WgPGaCcOQ">
+    <Link href="https://www.youtube.com/@imkarimkarim">
       <img class={imageClass} src={youtube} alt="youtube icon" />
     </Link>
   </div>
