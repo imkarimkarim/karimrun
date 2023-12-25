3719acd807e0c5f3156cd7d921e9ef027f0ff95e
imkarimkarim <imkarimkarim@pm.me>
2023-12-25 14:57:27 +0330

    feat: add resume file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import image from "@astrojs/image";
+import image from '@astrojs/image';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image(), tailwind()]
+	integrations: [image(), tailwind()],
+	redirects: {
+		'/resume': {
+			status: 302,
+			destination: '/karim-resume.pdf',
+		},
+	},
 });
