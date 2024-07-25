b0ea2858ef0dc70b0ce2b0252af4148d71337845
imkarimkarim <imkarimkarim@pm.me>
2024-07-25 22:01:24 +0330

    💥 switch to eAntillon/void-astro theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,24 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
-// https://astro.build/config
-import image from '@astrojs/image';
-
-// https://astro.build/config
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [image(), tailwind()],
-	redirects: {
-		'/resume': {
-			status: 302,
-			destination: '/karim-cv.pdf',
-		},
-		'/cv': {
-			status: 302,
-			destination: '/karim-cv.pdf',
-		},
-	},
-});
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), tailwind()],
+    markdown: {
+        shikiConfig: {
+          // Choose from Shiki's built-in themes (or add your own)
+          // https://shiki.style/themes
+          // Alternatively, provide multiple themes
+          // See note below for using dual light/dark themes
+          themes: {
+            light: 'poimandres',
+            dark: 'catppuccin-latte',
+          },
+        },
+      },
+    
+})
