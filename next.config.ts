bdd728ed45673e71da6b35e996447c6341e3906d
imkarimkarim <imkarimkarim@pm.me>
2024-12-09 23:15:20 +0330

    fix: redirect

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/cv",
-        destination: "/karim-cv.pdf",
-        permanent: false,
-      },
-    ];
-  },
   output: "export",
   eslint: {
     ignoreDuringBuilds: true,
