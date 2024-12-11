181cd43ae37e897189c6056debadc3fe925c532e
imkarimkarim <imkarimkarim@pm.me>
2024-12-11 08:18:23 +0330

    Revert "fix: redirect"
    This reverts commit bdd728ed45673e71da6b35e996447c6341e3906d.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/cv",
+        destination: "/karim-cv.pdf",
+        permanent: false,
+      },
+    ];
+  },
   output: "export",
   eslint: {
     ignoreDuringBuilds: true,
