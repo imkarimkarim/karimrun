aeb809c67870c2b990a36a199d02a7c3ea8b5447
imkarimkarim <imkarimkarim@pm.me>
2024-12-25 11:29:56 +0330

    chore

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'karim, internet crawler 🔭'
-export const SITE_DESCRIPTION =  `internet crawler, Front-End developer and GNU/Linux user
+export const SITE_TITLE = 'karim, internet explorer 🔭'
+export const SITE_DESCRIPTION =  `internet explorer, Front-End developer and GNU/Linux user
 with 4 years of experience in building and maintaining
 web applications. I have experience working in teams
 and collaborating with others to ship production software.
