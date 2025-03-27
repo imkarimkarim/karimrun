ef81b0f9843da56996cdb49573419129653ae4f4
imkarimkarim <imkarimkarim@pm.me>
2025-03-27 08:41:15 +0330

    --allow-empty-message

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Alireza Madani, a.k.a karim'
+export const SITE_TITLE = 'karim, internet crawler 🔭'
 export const SITE_DESCRIPTION =  `a internet crawler 🔭, and GNU/Linux user`;
